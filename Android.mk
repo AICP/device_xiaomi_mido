818f41e82c4a27969ca5c263115efff70a33c401
qjohn <townex22@gmail.com>
2018-03-20 03:03:36 +0700

    mido: Update makefile
    Change-Id: Ibb0b88e6e3c1e1935aea90fab827798fb1c4caf3

--- Android.mk
+++ Android.mk
@@ -14,10 +14,18 @@
 # limitations under the License.
 #
 
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter mido,$(TARGET_DEVICE)),)
-
+ifeq ($(TARGET_DEVICE),mido)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
